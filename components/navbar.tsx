4a7ab10899910b8d8494d6acbaa2fb5265432ee8
Vishal <vishalsharma05052002@gmail.com>
2024-10-11 17:10:42 +0530

    added the github button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
-
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -10,7 +10,12 @@ export default function Navbar() {
        
           <span className="font-bold ml-2 text-2xl">HeroJunction</span>
         </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
+        <nav className="ml-auto flex gap-3 sm:gap-6">
+        <Link href="https://github.com/Vishal2002/herojunction" target="_blank" rel="noopener noreferrer">
+            <Button variant="ghost" size="sm" className="h-8 gap-2">
+              <GitHubLogoIcon className="h-5 w-5" />
+            </Button>
+          </Link>
           <Link href="https://x.com/sharma_188">
             <Button variant="ghost" className="h-9 w-9 px-0">
               <Image src="/x.png" alt="X (Twitter) Icon" width={20} height={20} />
